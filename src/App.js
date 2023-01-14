c7f0cc94b284aa5eae461760fc14a69926112f1c
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2023-01-14 04:18:40 -0500

    feat: create initial structure of controller body

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './App.css';
 
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">10°</div>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
